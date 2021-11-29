7e25b05f0361347f3cad7164f8f69afb4fc27496
f-caro <fa.caro.aguilera@gmail.com>
2021-11-29 18:05:33 -0300

    described options to connect to AP or Wifi

--- boot.py
+++ boot.py
@@ -4,4 +4,5 @@
 import webrepl
 webrepl.start()
 #import pulse_detector
-import wlan_sta_init
+#import wlan_sta_init   #Connect to local wifi, and listens on static IP address
+import wlan_ap_init		#Creates an AccessPoint, letting user connect directly to webserver
