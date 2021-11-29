0bb75e52141c72d5a10fa9f541d6e6abde2db3ae
f-caro <fa.caro.aguilera@gmail.com>
2021-11-29 18:07:36 -0300

    need to enable pulse_detector script to start

--- boot.py
+++ boot.py
@@ -3,6 +3,6 @@
 #esp.osdebug(None)
 import webrepl
 webrepl.start()
-#import pulse_detector
+import pulse_detector
 #import wlan_sta_init   #Connect to local wifi, and listens on static IP address
 import wlan_ap_init		#Creates an AccessPoint, letting user connect directly to webserver
